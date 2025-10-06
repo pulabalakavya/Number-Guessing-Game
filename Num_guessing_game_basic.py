4478b89c71e09c41d47d22ea11b33ef0a4c321da
Kavya Pulabala <kavyapulabala@gmail.com>
2025-10-06 20:51:40 +0530

    simple number guessing game

---
+++ Num_guessing_game_basic.py
@@ -0,0 +1,16 @@
+import random
+
+number_to_guess=random.randint(1,100)
+while True:
+    try:
+        guess=int(input('Guess the number between 1 to 100:'))
+
+        if guess < number_to_guess:
+            print('Too Low..!')
+        elif guess > number_to_guess:
+            print('Too High..!')
+        else:
+            print('Congratulations ! You guessed the number.')
+            break
+    except ValueError:
+        print('Please enter a valid number')                
