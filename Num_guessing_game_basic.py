fc2530c2c97b56c8f13f288a845fd5588dc2667a
Kavya Pulabala <kavyapulabala@gmail.com>
2025-10-06 21:01:46 +0530

    updated game to include number of attempts

--- Num_guessing_game_basic.py
+++ Num_guessing_game_basic.py
@@ -1,9 +1,11 @@
 import random
 
 number_to_guess=random.randint(1,100)
+attempts =0
 while True:
     try:
         guess=int(input('Guess the number between 1 to 100:'))
+        attempts +=1
 
         if guess < number_to_guess:
             print('Too Low..!')
